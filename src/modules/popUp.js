5c4537a6b7ae6d2d771be5e607324c5f8b3ba553
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-07-05 00:34:49 -0400

    Add pop up form and atyles, saving data is not working

--- src/modules/popUp.js
+++ src/modules/popUp.js
@@ -1,6 +1,42 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-console */
 import { fetchData } from './api.js';
 
-const showComments = (item) => {
+const createCommentForm = (item) => {
+  const form = document.createElement('form');
+  form.className = 'comment-form';
+
+  const commentInput = document.createElement('textarea');
+  commentInput.placeholder = 'Enter your comment';
+  commentInput.className = 'comment-input';
+  form.appendChild(commentInput);
+
+  const submitButton = document.createElement('button');
+  submitButton.type = 'submit';
+  submitButton.textContent = 'Submit';
+  submitButton.className = 'submit-button';
+  form.appendChild(submitButton);
+
+  // Manejar el evento de envío del formulario
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const commentText = commentInput.value;
+    const savedComment = await saveComment(item, commentText);
+    // eslint-disable-next-line no-undef
+    displayComment(savedComment, commentsSection);
+    commentInput.value = '';
+  });
+
+  return form;
+};
+
+const displayComment = (comment, commentsSection) => {
+  const commentElement = document.createElement('div');
+  commentElement.textContent = comment.comment;
+  commentsSection.appendChild(commentElement);
+};
+
+const showComments = async (item) => {
   const popupContainer = document.createElement('div');
   popupContainer.classList.add('popup-container');
 
@@ -19,23 +55,55 @@ const showComments = (item) => {
   commentsSection.classList.add('comments-section');
   popupContent.appendChild(commentsSection);
 
-  const commentsUrl = `https://api.tvmaze.com/shows/${item.id}/comments`;
-  fetchData(commentsUrl)
-    .then((comments) => {
-      comments.forEach((comment) => {
-        const commentElement = document.createElement('div');
-        commentElement.textContent = comment.text;
-        commentsSection.appendChild(commentElement);
-      });
+  const commentsUrl = `https://api.tvmaze.com/shows/${item}`;
 
-      popupContainer.appendChild(popupContent);
-      const mainElement = document.querySelector('main');
-      mainElement.appendChild(popupContainer);
-    })
-    .catch((error) => {
-      // eslint-disable-next-line no-console
-      console.error('Error fetching comments:', error);
+  try {
+    const comments = await fetchData(commentsUrl);
+
+    const commentElement = document.createElement('div');
+    commentElement.textContent = comments.name;
+    commentsSection.appendChild(commentElement);
+
+    const imageElement = document.createElement('img');
+    imageElement.src = comments.image;
+    commentElement.appendChild(imageElement);
+
+    const commentForm = createCommentForm(item);
+    popupContent.appendChild(commentForm);
+
+    popupContainer.appendChild(popupContent);
+    const mainElement = document.querySelector('main');
+    mainElement.appendChild(popupContainer);
+  } catch (error) {
+    console.error('Error fetching comments:', error);
+  }
+};
+
+const saveComment = async (itemId, commentText) => {
+  const commentData = {
+    item_id: itemId,
+    comment: commentText,
+  };
+
+  try {
+    const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/c26WXJgsOY60FiNtcTNS/comments/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(commentData),
     });
+
+    if (!response.ok) {
+      throw new Error('Failed to save comment');
+    }
+
+    const savedComment = await response.json();
+    return savedComment;
+  } catch (error) {
+    console.error('Error saving comment:', error);
+    throw error;
+  }
 };
 
 export default showComments;
