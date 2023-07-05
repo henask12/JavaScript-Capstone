490a12c52dde1ff86fce73f6807c7ce43810843b
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-07-05 13:55:58 -0400

    Fix popup scroll and image issue

--- src/modules/popUp.js
+++ src/modules/popUp.js
@@ -2,29 +2,40 @@
 /* eslint-disable no-console */
 import { fetchData } from './api.js';
 
-const createCommentForm = (item) => {
+const createCommentForm = (itemId, commentsSection) => {
   const form = document.createElement('form');
   form.className = 'comment-form';
 
+  const formTitle = document.createElement('h2');
+  formTitle.textContent = 'Add Your Comment';
+  form.appendChild(formTitle);
+
+  const nameInput = document.createElement('input');
+  nameInput.placeholder = 'Your name';
+  nameInput.type = 'text';
+  nameInput.className = 'name-input';
+  form.appendChild(nameInput);
+
   const commentInput = document.createElement('textarea');
-  commentInput.placeholder = 'Enter your comment';
+  commentInput.placeholder = 'Your insights';
   commentInput.className = 'comment-input';
   form.appendChild(commentInput);
 
   const submitButton = document.createElement('button');
   submitButton.type = 'submit';
-  submitButton.textContent = 'Submit';
+  submitButton.textContent = 'Comment';
   submitButton.className = 'submit-button';
   form.appendChild(submitButton);
 
   // Manejar el evento de envío del formulario
   form.addEventListener('submit', async (event) => {
     event.preventDefault();
+    const name = nameInput.value;
     const commentText = commentInput.value;
-    const savedComment = await saveComment(item, commentText);
-    // eslint-disable-next-line no-undef
+    const savedComment = await saveComment(itemId, name, commentText);
     displayComment(savedComment, commentsSection);
     commentInput.value = '';
+    nameInput.value = '';
   });
 
   return form;
@@ -32,7 +43,15 @@ const createCommentForm = (item) => {
 
 const displayComment = (comment, commentsSection) => {
   const commentElement = document.createElement('div');
-  commentElement.textContent = comment.comment;
+
+  const usernameElement = document.createElement('strong');
+  usernameElement.textContent = comment.username;
+  commentElement.appendChild(usernameElement);
+
+  const textElement = document.createElement('p');
+  textElement.textContent = comment.comment;
+  commentElement.appendChild(textElement);
+
   commentsSection.appendChild(commentElement);
 };
 
@@ -59,34 +78,35 @@ const showComments = async (item) => {
 
   try {
     const comments = await fetchData(commentsUrl);
+    console.log(comments);
+
+    const commentForm = createCommentForm(item, commentsSection);
+    popupContent.appendChild(commentForm);
 
     const commentElement = document.createElement('div');
     commentElement.textContent = comments.name;
     commentsSection.appendChild(commentElement);
 
     const imageElement = document.createElement('img');
-    imageElement.src = comments.image;
+    imageElement.src = comments.image ? comments.image.medium : '';
     commentElement.appendChild(imageElement);
 
-    const commentForm = createCommentForm(item);
-    popupContent.appendChild(commentForm);
-
     popupContainer.appendChild(popupContent);
-    const mainElement = document.querySelector('main');
-    mainElement.appendChild(popupContainer);
+    document.body.appendChild(popupContainer);
   } catch (error) {
     console.error('Error fetching comments:', error);
   }
 };
 
-const saveComment = async (itemId, commentText) => {
+const saveComment = async (itemId, name, commentText) => {
   const commentData = {
     item_id: itemId,
+    username: name,
     comment: commentText,
   };
 
   try {
-    const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/c26WXJgsOY60FiNtcTNS/comments/', {
+    const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/c26WXJgsOY60FiNtcTNS/comments', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
